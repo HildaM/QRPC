179b9ed556d531d98e07fb86efcb73ee590a1a5a
Quan <Hilda_quan@163.com>
2022-08-18 17:11:29 +0800

    Day03: 服务注册

---
+++ reflect/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"reflect"
+	"strings"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	typ := reflect.TypeOf(&wg)
+
+	// 通过反射遍历wg下的所有方法
+	for i := 0; i < typ.NumMethod(); i++ {
+		method := typ.Method(i)
+		argv := make([]string, 0, method.Type.NumIn()) // make([]string, len, cap)
+		returns := make([]string, 0, method.Type.NumIn())
+
+		// j从 1 开始，第 0 个入参是 wg 自己
+		for j := 1; j < method.Type.NumIn(); j++ {
+			argv = append(argv, method.Type.In(j).Name())
+		}
+		for j := 0; j < method.Type.NumOut(); j++ {
+			returns = append(returns, method.Type.Out(j).Name())
+		}
+
+		log.Printf("func (w *%s) %s(%s) %s",
+			typ.Elem().Name(),
+			method.Name,
+			strings.Join(argv, ","), // 用逗号分割
+			strings.Join(returns, ","),
+		)
+	}
+}
